5b1fe8c8bfd16553ddb7ee4a7c597fc02ee32164
venkat-veer <s.venkatveer@gmail.com>
2025-09-27 23:41:40 +0530

    reduce-code-mongo

--- common.sh
+++ common.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 MONGODB_HOST=mongodb.devaws.store
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
