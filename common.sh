b79a21b135dde953329b2d0c3f85c76d7d1ae080
venkat-veer <s.venkatveer@gmail.com>
2025-09-28 17:22:04 +0530

    rectify-shipping-code

--- common.sh
+++ common.sh
@@ -10,8 +10,10 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD # for absolute path
 MONGODB_HOST=mongodb.devaws.store
-SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.devaws.store
+
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
